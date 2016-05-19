1c9673bf7d5f5a4b7396ebc7c4ebb230e1c9423e
Jim de Beer <jim@vigour.io>
2016-05-20 00:09:50 +0200

    no $remove subs for $parent and $root

--- test/remove.js
+++ test/remove.js
@@ -22,11 +22,6 @@ test('remove', (t) => {
 test('remove - path subscription', (t) => {
   const state = s({ first: { second: 'a' } })
   const app = render({
-    more: {
-      tag: 'p',
-      $: 'first',
-      text: { $: 'second' }
-    },
     first: {
       second: {
         tag: 'h1',
@@ -34,7 +29,23 @@ test('remove - path subscription', (t) => {
       }
     }
   }, state)
-  t.equal(p(app), '<div><p>a</p><div><h1/></div></div>', 'correct initial html')
+  t.equal(p(app), '<div><div><h1/></div></div>', 'correct initial html')
+  state.first.remove()
+  t.equal(p(app), '<div><div/></div>', 'removed node')
+  t.end()
+})
+
+test('remove - path subscription - $root', (t) => {
+  const state = s({ first: { second: 'a' } })
+  const app = render({
+    first: {
+      second: {
+        tag: 'h1',
+        $: '$root.first.second'
+      }
+    }
+  }, state)
+  t.equal(p(app), '<div><div><h1/></div></div>', 'correct initial html')
   state.first.remove()
   t.equal(p(app), '<div><div/></div>', 'removed node')
   t.end()
@@ -54,6 +65,37 @@ test('remove - mixed and context', (t) => {
       }
     },
     text: { $: 'first.second' },
+    basic: { type: 'basic' }
+  }, state)
+
+  t.equal(
+    p(app),
+    '<div>a<p><div>a</div><div>b</div></p></div>',
+    'correct initial html'
+  )
+  state.first.remove()
+  t.equal(
+    p(app),
+    '<div><p><div>b</div></p></div>',
+    'removed node'
+  )
+  t.end()
+})
+
+test('remove - mixed and context - $root', (t) => {
+  const state = s({ first: { second: 'a' } })
+  const app = render({
+    components: {
+      basic: {
+        tag: 'p',
+        first: {
+          $: '$root.first',
+          text: { $: 'second' }
+        },
+        second: { text: 'b' }
+      }
+    },
+    text: { $: '$root.first.second' },
     basic: { type: 'basic' }
   }, state)
 
