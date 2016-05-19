8ff41a3235f6b50686120acbc3b9a1c6166c8690
Jim de Beer <jim@vigour.io>
2016-05-19 13:03:07 +0200

    renamed node to tag

--- test/collection.js
+++ test/collection.js
@@ -1,15 +1,22 @@
-// 'use strict'
-// const render = require('../render')
-// const test = require('tape')
+'use strict'
+const render = require('../render')
+const test = require('tape')
 
-// test('collection', function (t) {
-//   // types
-//   var app = {
-//     holder: {
-//       $: 'collection.$any',
-//       Child: {
-//         title: { $: 'title' }
-//       }
-//     }
-//   }
-// })
+test('collection', function (t) {
+  // types
+  var app = {
+    holder: {
+      $: 'collection.$any',
+      Child: {
+        tag: 'span',
+        title: { $: 'title' }
+      }
+    }
+  }
+  console.log(render(app, {
+    collection: [
+      { title: 'a' },
+      { title: 'b' }
+    ]
+  })).toString()
+})
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
 'use strict'
 require('./map')
 require('./group')
+require('./collection')
