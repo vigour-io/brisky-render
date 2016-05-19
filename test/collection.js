6d58cbb253766f63251b46c33283bc7780931207
Jim de Beer <jim@vigour.io>
2016-05-19 15:42:54 +0200

    added fixes

--- test/collection.js
+++ test/collection.js
@@ -4,22 +4,39 @@ const test = require('tape')
 
 test('collection', function (t) {
   // types
-  var app = {
+  const app = {
     holder: {
       $: 'collection.$any',
       Child: {
         tag: 'span',
-        title: { $: 'title' }
+        title: { $: 'title' },
+        input: {
+          tag: 'input'
+          // props: {
+            // value: 'hello'
+          // }
+        },
+        on: {
+          click (e) {}
+        }
       }
     }
   }
-  console.log(render(app, {
+
+  const elem = render(app, {
     collection: [
       { title: 'a' },
       { title: 'b' }
     ]
-  }).innerHTML)
+  })
 
+  console.log(elem.fastRender())
+
+  var d = Date.now()
+  // for (let i = 0; i < 1e4; i++) {
+  //   elem.innerHTML
+  // }
+  console.log(Date.now() - d + 'ms')
   t.end()
   // need to exclude those order fields...
   // find an easy solution for that -- maybe if isNode -- define props?
