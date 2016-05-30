0c2d4f3768e12f4ecdf66bcb57ad3cd007bde73d
Jim de Beer <jim@vigour.io>
2016-05-30 17:02:15 +0200

    fixed all tests, fixed order

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 'use strict'
 require('./order')
-// require('./html')
-// require('./group')
-// require('./text')
+require('./html')
+require('./group')
+require('./text')
 // require('./remove')
 // require('./widget')
 // require('./collection')
