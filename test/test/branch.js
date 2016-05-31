9447cd3c3e8ceda5d514a5e1d8868f87156369d7
Jim de Beer <jim@vigour.io>
2016-05-31 19:21:13 +0200

    branching tests

--- test/test/branch.js
+++ test/test/branch.js
@@ -6,44 +6,82 @@ const parse = require('parse-element')
 const s = require('vigour-state/s')
 
 test('$test - branch', function (t) {
+  const state = s({ fields: { first: true } })
   var appState
   var app = render({
     holder: {
+      tag: 'holder',
       $: 'fields',
       first: {
+        tag: 'first',
         $: '$test',
-        $test: {
-          val (state) {
-            return 'swiggy' in state && state.swiggy.compute() === true
-          },
-          $: {
-            title: {}
-          }
+        $test (state) {
+          return 'first' in state &&
+            state.first.compute() === true &&
+            state.get('second', {}).compute() !== true
+        }
+      },
+      second: {
+        tag: 'second',
+        $: '$test',
+        $test (state) {
+          return 'second' in state && state.second.compute() === true
         }
       }
-      // second: {
-      //   $: '$test',
-      //   $test: {
-      //     val (state) {
-      //       return state.title.compute() === 'a'
-      //     },
-      //     $: {
-      //       title: {}
-      //     }
-      //   }
-      // }
+    },
+    third: {
+      tag: 'third',
+      $: 'fields.$test',
+      $test: {
+        val (state) {
+          let $r = state.getRoot()
+          return 'third' in $r && $r.third.compute() === true
+        },
+        $: '$root.third'
+      }
     }
   },
-  s({
-    fields: {
-      swiggy: true
-    }
-  }),
+  state,
   (subs) => {
     if (!appState) {
       appState = subs
     }
   })
-  console.log(parse(app))
+
+  t.same(
+    parse(app),
+    '<div><holder><first></first></holder></div>',
+    'correct html on intial state'
+  )
+  state.fields.set({ first: false })
+  t.same(
+    parse(app),
+    '<div><holder></holder></div>',
+    'set state.fields.first to false'
+  )
+  state.fields.set({ second: true })
+  t.same(
+    parse(app),
+    '<div><holder><second></second></holder></div>',
+    'set state.fields.second to true'
+  )
+  state.fields.set({ first: true })
+  t.same(
+    parse(app),
+    '<div><holder><second></second></holder></div>',
+    'set state.fields.first to true, dont show since second is true'
+  )
+  state.fields.set({ second: false })
+  t.same(
+    parse(app),
+    '<div><holder><first></first></holder></div>',
+    'set state.fields.second to false, show first'
+  )
+  state.set({ third: true })
+  t.same(
+    parse(app),
+    '<div><holder><first></first></holder><third></third></div>',
+    'set state.third to true, show third'
+  )
   t.end()
 })
