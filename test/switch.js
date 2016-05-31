17a52175c9b0e560dc38f2dec91ed0c0b35876dc
Jim de Beer <jim@vigour.io>
2016-05-31 20:05:32 +0200

    start of tests for switch double -- redid map

--- test/switch.js
+++ test/switch.js
@@ -3,54 +3,104 @@ const render = require('../render')
 const test = require('tape')
 const parse = require('parse-element')
 const s = require('vigour-state/s')
+const strip = require('vigour-util/strip/formatting')
 
 test('switch - branch', function (t) {
-  const state = s({ navigation: {} })
+  const state = s({ field: { navigation: {} } })
 
   var app = render(
     {
       holder: {
         tag: 'holder',
-        $: 'navigation.$switch',
-        $switch (state, type, stamp, subs, tree, sType) {
-          console.log('lullzors', state.key)
-          return state.key
-        },
-        properties: {
-          first: {
-            tag: 'first',
-            text: { $: 'title' }
-          },
-          second: {
-            tag: 'second',
-            text: { $: 'rating' }
+        switcher: {
+          tag: 'switcher',
+          $: 'field.navigation.$switch',
+          $switch: (state) => state.key,
+          properties: {
+            first: {
+              tag: 'first',
+              text: { $: 'title' }
+            },
+            second: {
+              tag: 'second',
+              text: { $: 'rating' }
+            }
           }
         }
+        // THIS BREAKS ORDER FIX IT!
+        // second: {
+        //   tag: 'second',
+        //   $: 'field',
+        //   switcher: {
+        //     // $: 'navigation.$switch',
+        //     // $switch: (state) => state.key
+        //   }
+        // }
+        // in here it breaks
+      },
+      holder2: {
+        tag: 'holder2',
+        $: 'field'
+        // switcher: {
+          // $: 'navigation.$switch',
+          // $switch: (state) => state.key
+        // }
       }
     },
     state
   )
 
-  t.equal(parse(app), '<div><holder></holder></div>', 'intial')
+  t.equal(
+    parse(app),
+    strip(`
+      <div>
+        <holder>
+          <switcher></switcher>
+        </holder>
+        <holder2></holder2>
+      </div>
+    `),
+    'intial'
+  )
 
   state.set({
     items: {
       first: { title: 'first' },
       second: { rating: 100 }
     },
-    navigation: '$root.items[0]'
+    field: {
+      navigation: '$root.items[0]'
+    }
   })
 
   t.equal(
     parse(app),
-    '<div><holder><first>first</first></holder></div>',
+    strip(`
+      <div>
+        <holder>
+          <switcher>
+            <first>first</first>
+          </switcher>
+        </holder>
+        <holder2></holder2>
+      </div>
+    `),
     'switch navigation to items[0]'
   )
 
-  state.navigation.set('$root.items[-1]')
+  state.field.navigation.set('$root.items[-1]')
   t.equal(
     parse(app),
-    '<div><holder><second>100</second></holder></div>',
+     strip(`
+      <div>
+        <holder>
+          <switcher>
+            <second>100</second>
+          </switcher>
+        </holder>
+        <holder2></holder2>
+      </div>
+    `),
     'switch navigation to items[1]'
   )
   t.end()
