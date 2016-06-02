bcb99da024ee9d66bf0b41eae472720644d67d24
Jim de Beer <jim@vigour.io>
2016-06-02 15:46:28 +0200

    fixing order

---
+++ test/fragment.js
@@ -0,0 +1,55 @@
+'use strict'
+const render = require('../render')
+const test = require('tape')
+// const parse = require('parse-element')
+const s = require('vigour-state/s')
+// const strip = require('vigour-util/strip/formatting')
+// const Element = require('../lib/element')
+
+test('fragment', function (t) {
+  const state = global.state = s()
+
+  const types = {
+    fragment: {
+      tag: 'fragment',
+      $: 'lulz',
+      text: '-----------'
+      // a: { text: 'hello' },
+      // b: { text: 'blurgh' },
+      // c: { text: { $: true } },
+      // footer: { type: 'text', val: '----------' }
+    }
+  }
+
+  const app = render(
+    {
+      types,
+      text: 'app',
+      holder: {
+        $: 'lol',
+        frag: { type: 'fragment' },
+        statics: {
+          text: 'its static!'
+        }
+      }
+    },
+    state
+  )
+
+  setTimeout(function () {
+    state.set({
+      lol: {
+        lulz: 'lulz!'
+      }
+    })
+  }, 500)
+
+  document.body.appendChild(app)
+
+  // setTimeout(function () {
+  //   console.log('remove fragment!')
+  //   state.lol.lulz.remove()
+  // }, 500)
+
+  t.end()
+})
