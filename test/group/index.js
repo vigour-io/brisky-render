38deb49bbaa923b59d52ee97500e91d384f1da2b
Jim de Beer <jim@vigour.io>
2016-06-10 18:31:08 +0200

    increase n

--- test/group/index.js
+++ test/group/index.js
@@ -52,12 +52,12 @@ test('group', function (t) {
     },
     child: {
       tag: 'span',
-      $: '$test',
-      $test: (state) => {
-        const x = state[0] && state[0].compute()
-        const y = state[1] && state[1].compute()
-        return x > max / 2 && y > max / 2 && x < max * 10 && y < max * 5
-      },
+      // $: '$test',
+      // $test: (state) => {
+      //   const x = state[0] && state[0].compute()
+      //   const y = state[1] && state[1].compute()
+      //   return x > max / 2 && y > max / 2 && x < max * 10 && y < max * 5
+      // },
       title: {
         tag: 'h1',
         $: 'title',
