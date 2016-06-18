92772833d5c3765d560bf3e44b81783764938f61
Youri Daamen <me@youz.io>
2016-06-18 09:57:02 +0100

    player drm modal

---
+++ test/parent.js
@@ -0,0 +1,21 @@
+'use strict'
+const render = require('../render')
+const test = require('tape')
+const p = require('parse-element')
+const s = require('vigour-state/s')
+
+test('parent', (t) => {
+  console.log('lets go parent')
+  const state = s({ first: true })
+  // add broken operator case and everything
+  const app = render({
+    first: {
+      tag: 'h1',
+      $: 'first'
+    }
+  }, state)
+  t.equal(p(app), '<div><h1></h1></div>', 'correct initial html')
+  state.first.remove()
+  t.equal(p(app), '<div></div>', 'removed node')
+  t.end()
+})
