46190d261f4a6f6b49423bb07ac959a54b86d365
Jim de Beer <jim@vigour.io>
2016-07-03 11:06:23 +0200

    added parent error test

--- test/parent.js
+++ test/parent.js
@@ -3,6 +3,7 @@ const render = require('../render')
 const test = require('tape')
 const p = require('parse-element')
 const s = require('vigour-state/s')
+const strip = require('vigour-util/strip/formatting')
 
 test('parent', (t) => {
   const state = s({ first: true })
@@ -16,4 +17,29 @@ test('parent', (t) => {
   state.first.remove()
   t.equal(p(app), '<div></div>', 'removed node')
   t.end()
+})
+
+test('parent - error', (t) => {
+  var tree
+  const state = s({})
+  render({
+    first: {
+      tag: 'h1',
+      text: { $: 'first' }
+    }
+  }, state, (s, t) => {
+    tree = t
+  })
+
+  delete tree._[210]
+  try {
+    state.set({ first: 'hello' })
+  } catch (e) {
+    t.equal(
+      strip(e.message),
+      strip('No parent in treetype: "new" pid: "210"path: "first"'),
+      'correct error message'
+    )
+    t.end()
+  }
 })
