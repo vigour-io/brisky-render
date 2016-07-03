d4cabeb42c688a239be4cef5999a10fd040206b3
Jim de Beer <jim@vigour.io>
2016-07-03 10:30:29 +0200

    added tests for top level subscribes, added attach and id closes #6

--- test/render.js
+++ test/render.js
@@ -3,8 +3,9 @@ const render = require('../render')
 const test = require('tape')
 const s = require('vigour-state/s')
 const p = require('parse-element')
+const Observable = require('vigour-observable')
 
-test('render', (t) => {
+test('render - id, callback', (t) => {
   const state = s({
     field: 'its text'
   })
@@ -17,7 +18,7 @@ test('render', (t) => {
     },
     state,
     () => { cnt++ },
-    void 0,
+    false,
     'someId'
   )
   t.equal(p(app), '<div>its text</div>', 'initial')
@@ -25,5 +26,53 @@ test('render', (t) => {
   t.equal(p(app), '<div>update</div>', 'fires update')
   t.same(state.emitters.subscription.fn.keys(), [ 'someId' ], 'adds listener to correct id')
   t.equal(cnt, 3, 'fired callback for each render')
+  t.end()
+})
+
+test('render - attach + $any on top', (t) => {
+  const state = s([ 1, 2 ])
+  const b = new Observable()
+  const app = render(
+    {
+      $: '$any',
+      tag: 'ul',
+      child: {
+        tag: 'li',
+        text: { $: true }
+      }
+    },
+    state,
+    false,
+    b
+  )
+  t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
+  t.same(state.emitters.subscription.attach.keys(), [ 1 ], 'adds attach-listener')
+  b.remove()
+  t.same(state.emitters.subscription.attach.keys(), [], 'removes attach-listener')
+  t.end()
+})
+
+test('render - $switch on top', (t) => {
+  const state = s({
+    text: ':/',
+    html: ':)'
+  })
+  const app = render(
+    {
+      $: 'a.$switch',
+      $switch: (state) => state.key,
+      properties: {
+        text: { type: 'text', $: true },
+        html: { type: 'element', html: { $: true } }
+      }
+    },
+    state
+  )
+  t.equal(p(app), '<div></div>', 'initial')
+  t.ok('a' in state, 'created field on init')
+  state.a.set('$root.text')
+  t.equal(p(app), '<div>:/</div>', 'swtich')
+  state.a.set('$root.html')
+  t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
   t.end()
 })
