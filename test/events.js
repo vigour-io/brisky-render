ff0aa6ce825ba5c80e8b5217d7c4b08ad7962b99
Jim de Beer <jim@vigour.io>
2016-07-11 11:33:28 +0200

    remove all special behaviour for hasEvents

--- test/events.js
+++ test/events.js
@@ -8,7 +8,6 @@ test('events', (t) => {
   const state = s({ something: true })
   const elem = new Element({
     node: {
-      tag: 'thing',
       $: 'something',
       define: { hasEvents: true }
     }
@@ -23,7 +22,6 @@ test('events - deep', (t) => {
   const state = s({ something: true })
   const elem = new Element({
     node: {
-      tag: 'thing',
       $: 'something',
       hello: {
         bla: {
@@ -35,5 +33,29 @@ test('events - deep', (t) => {
   const app = render(elem, state)
   t.equal(app.childNodes[0].childNodes[0].childNodes[0]._, elem.node.hello.bla, 'correct _ on node')
   t.equal(app.childNodes[0].childNodes[0].childNodes[0]._s, state.something, 'correct state on node')
+  t.end()
+})
+
+test('events - reference', (t) => {
+  const state = s({
+    a: { field: true },
+    b: { field: true },
+    something: '$root.a'
+  })
+  const elem = new Element({
+    node: {
+      $: 'something',
+      field: {
+        $: 'field',
+        define: { hasEvents: true }
+      }
+    }
+  })
+  const app = render(elem, state)
+  t.equal(app.childNodes[0].childNodes[0]._s, state.a.field, 'correct state on node')
+
+  console.log('lets go')
+  state.something.set('$root.b')
+  t.equal(app.childNodes[0].childNodes[0]._s, state.b.field, 'correct state on node after set')
   t.end()
 })
