93f021439e19cb7af4226cb229a6feec04472faa
Jim de Beer <jim@vigour.io>
2016-07-19 16:01:20 +0300

    fixed clonenode static inheritance problem, closes #30

---
+++ test/clone.js
@@ -0,0 +1,60 @@
+'use strict'
+const render = require('../render')
+const test = require('tape')
+const s = require('vigour-state/s')
+const browser = require('./browser')
+const getParent = require('../lib/render/dom/parent')
+
+test('clone - reuse ', function (t) {
+  const state = global.state = s({
+    one: {
+      a: 'one',
+      b: 'http://cdn.images.express.co.uk/img/dynamic/1/590x/secondary/Cat-536495.jpg'
+    },
+    two: {
+      a: 'two'
+    }
+  })
+
+  const app = browser(render({
+    types: {
+      img: {
+        title: { text: { $: 'a' } },
+        img: {
+          tag: 'img',
+          class: {
+            type: 'property',
+            render: {
+              static (target, node) {
+                node.className = target.parent.parent.key
+              }
+            }
+          },
+          src: {
+            type: 'property',
+            $: 'b',
+            render: {
+              state (target, state, type, stamp, subs, tree, id, pid, store) {
+                const node = getParent(type, stamp, subs, tree, pid)
+                node.src = state.compute()
+              }
+            }
+          }
+        }
+      }
+    },
+    one: {
+      type: 'img',
+      $: 'two'
+    },
+    two: {
+      type: 'img',
+      $: 'one'
+    }
+  }, state))
+  var src = app.childNodes[0].childNodes[1].getAttribute('src')
+  if (src && src.value) { src = src.value }
+  if (!src) { src = void 0 }
+  t.equal(src, void 0, 'child .one does not inherit src')
+  t.end()
+})
