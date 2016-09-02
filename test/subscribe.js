b8551f5e8e4eec8cc1b07d1c90580f6c0e2d52a3
Jim de Beer <jim@vigour.io>
2016-09-02 20:52:15 +0200

    added fixes

--- test/subscribe.js
+++ test/subscribe.js
@@ -39,3 +39,37 @@ test('subscribe - resubscribe', (t) => {
   t.equal(p(app), '<div>silent update<a></a></div>', 'fires silent update')
   t.end()
 })
+
+test('subscribe - resubscribe - switch', (t) => {
+  const state = s({
+    a: 'its text',
+    b: 'its field2',
+    somefield: '$root.field'
+  })
+  const app = render(
+    {
+      switch: {
+        $: '$somefield.$switch',
+        $switch: (key) => key,
+        properties: {
+          a: { text: { $: true, $add: ' haha a' } },
+          b: { text: { $: true, $add: ' haha b' } }
+        }
+      }
+    },
+    state
+  )
+  state.field.set('update')
+  t.equal(p(app), '<div>update<a></a></div>', 'fires update')
+  state.field.val = 'silent update'
+  state.resubscribe()
+  t.equal(p(app), '<div>silent update<a></a></div>', 'fires silent update')
+
+  global.state = state
+
+  if (document.body) {
+    document.body.appendChild(app)
+  }
+
+  t.end()
+})
