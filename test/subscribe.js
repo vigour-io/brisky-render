59c69cf53bf03ca43b07aadc1c42013a1da4376b
Jim de Beer <jim@vigour.io>
2016-09-20 11:15:40 +0200

    added fixes

--- test/subscribe.js
+++ test/subscribe.js
@@ -4,64 +4,64 @@ const test = require('tape')
 const s = require('vigour-state/s')
 const p = require('parse-element')
 
-// test('subscribe - merge', (t) => {
-//   const state = s({
-//     field: 'its text'
-//   })
-//   const app = render(
-//     {
-//       text: { $: 'field' },
-//       a: { tag: 'a', $: 'field' }
-//     },
-//     state
-//   )
-//   t.equal(p(app), '<div>its text<a></a></div>', 'initial')
-//   state.field.set('update')
-//   t.equal(p(app), '<div>update<a></a></div>', 'fires update')
-//   t.end()
-// })
+test('subscribe - merge', (t) => {
+  const state = s({
+    field: 'its text'
+  })
+  const app = render(
+    {
+      text: { $: 'field' },
+      a: { tag: 'a', $: 'field' }
+    },
+    state
+  )
+  t.equal(p(app), '<div>its text<a></a></div>', 'initial')
+  state.field.set('update')
+  t.equal(p(app), '<div>update<a></a></div>', 'fires update')
+  t.end()
+})
 
-// test('subscribe - resubscribe', (t) => {
-//   const state = s({
-//     field: 'its text'
-//   })
-//   const app = render(
-//     {
-//       text: { $: 'field' },
-//       a: { tag: 'a', $: 'field' }
-//     },
-//     state
-//   )
-//   state.field.set('update')
-//   t.equal(p(app), '<div>update<a></a></div>', 'fires update')
-//   state.field.val = 'silent update'
-//   state.resubscribe()
-//   t.equal(p(app), '<div>silent update<a></a></div>', 'fires silent update')
-//   t.end()
-// })
+test('subscribe - resubscribe', (t) => {
+  const state = s({
+    field: 'its text'
+  })
+  const app = render(
+    {
+      text: { $: 'field' },
+      a: { tag: 'a', $: 'field' }
+    },
+    state
+  )
+  state.field.set('update')
+  t.equal(p(app), '<div>update<a></a></div>', 'fires update')
+  state.field.val = 'silent update'
+  state.resubscribe()
+  t.equal(p(app), '<div>silent update<a></a></div>', 'fires silent update')
+  t.end()
+})
 
-// test('subscribe - resubscribe - switch', (t) => {
-//   const state = s({
-//     a: 'its text',
-//     b: 'its field2',
-//     somefield: '$root.a'
-//   })
-//   const app = render({
-//     text: 'app',
-//     xxx: {
-//       tag: 'ul',
-//       $: 'somefield.$switch',
-//       $switch: (state) => state.key,
-//       properties: {
-//         a: { tag: 'li', text: { $: true, $add: ' haha a' } },
-//         b: { tag: 'li', text: { $: true, $add: ' haha b' } }
-//       }
-//     }
-//   }, state)
-//   state.resubscribe()
-//   t.equal(p(app), '<div>app<ul><li>its text haha a</li></ul></div>')
-//   t.end()
-// })
+test('subscribe - resubscribe - switch', (t) => {
+  const state = s({
+    a: 'its text',
+    b: 'its field2',
+    somefield: '$root.a'
+  })
+  const app = render({
+    text: 'app',
+    xxx: {
+      tag: 'ul',
+      $: 'somefield.$switch',
+      $switch: (state) => state.key,
+      properties: {
+        a: { tag: 'li', text: { $: true, $add: ' haha a' } },
+        b: { tag: 'li', text: { $: true, $add: ' haha b' } }
+      }
+    }
+  }, state)
+  state.resubscribe()
+  t.equal(p(app), '<div>app<ul><li>its text haha a</li></ul></div>')
+  t.end()
+})
 
 test('subscribe - object subscription', (t) => {
   const state = s({
