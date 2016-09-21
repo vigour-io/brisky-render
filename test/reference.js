30016fcfd2d418b612846137f904c0837b5e310b
Jim de Beer <jim@vigour.io>
2016-09-21 17:17:58 +0200

    re-enabled tests

--- test/reference.js
+++ test/reference.js
@@ -5,49 +5,49 @@ const parse = require('parse-element')
 const strip = require('vigour-util/strip/formatting')
 const s = require('vigour-state/s')
 
-// test('reference - basic', function (t) {
-//   const state = s({
-//     holder: {
-//       fields: {
-//         thing: 1
-//       },
-//       fields2: {
-//         thing: 2
-//       },
-//       current: '$root.holder.fields'
-//     }
-//   })
+test('reference - basic', function (t) {
+  const state = s({
+    holder: {
+      fields: {
+        thing: 1
+      },
+      fields2: {
+        thing: 2
+      },
+      current: '$root.holder.fields'
+    }
+  })
 
-//   const app = render({
-//     $: 'holder.current',
-//     page: {
-//       $: 'thing',
-//       text: { $: true }
-//     }
-//   }, state)
+  const app = render({
+    $: 'holder.current',
+    page: {
+      $: 'thing',
+      text: { $: true }
+    }
+  }, state)
 
-//   t.same(
-//     parse(app),
-//     strip(`
-//       <div>
-//         <div>1</div>
-//       </div>
-//     `)
-//   )
+  t.same(
+    parse(app),
+    strip(`
+      <div>
+        <div>1</div>
+      </div>
+    `)
+  )
 
-//   state.holder.current.set(state.holder.fields2)
+  state.holder.current.set(state.holder.fields2)
 
-//   t.same(
-//     parse(app),
-//     strip(`
-//       <div>
-//         <div>2</div>
-//       </div>
-//     `)
-//   )
+  t.same(
+    parse(app),
+    strip(`
+      <div>
+        <div>2</div>
+      </div>
+    `)
+  )
 
-//   t.end()
-// })
+  t.end()
+})
 
 test('reference - $any', function (t) {
   const state = s({
@@ -59,6 +59,53 @@ test('reference - $any', function (t) {
   })
   const app = render({
     $: 'holder.current',
+    page: {
+      $: '$any',
+      child: {
+        text: { $: true }
+      }
+    }
+  }, state, (subs) => {
+    console.log(subs, '?')
+  })
+  if (document && document.body) {
+    document.body.appendChild(app)
+  }
+  t.same(
+    parse(app),
+    strip(`
+      <div>
+        <div>
+          <div>1</div>
+          <div>2</div>
+        </div>
+      </div>
+    `)
+  )
+  console.error('\n\n\nok so what up in this bitch')
+  state.holder.current.set(state.holder.fields2)
+  t.same(
+    parse(app),
+    strip(`
+      <div>
+        <div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+      </div>
+    `)
+  )
+  t.end()
+})
+
+test('reference - root - $any', function (t) {
+  const state = s({
+    fields: [ 1, 2 ],
+    fields2: [ 3, 4 ],
+    current: '$root.fields'
+  })
+  const app = render({
+    $: 'current',
     page: {
       $: '$any',
       child: {
@@ -80,8 +127,7 @@ test('reference - $any', function (t) {
       </div>
     `)
   )
-  console.log('ok so what up in this bitch')
-  state.holder.current.set(state.holder.fields2)
+  state.current.set(state.fields2)
   t.same(
     parse(app),
     strip(`
@@ -95,43 +141,3 @@ test('reference - $any', function (t) {
   )
   t.end()
 })
-
-// test('reference - root - $any', function (t) {
-//   const state = s({
-//     fields: [ 1, 2 ],
-//     fields2: [ 3, 4 ],
-//     current: '$root.fields'
-//   })
-//   const app = render({
-//     $: 'holder.current',
-//     page: {
-//       $: '$any',
-//       child: {
-//         text: { $: true }
-//       }
-//     }
-//   }, state)
-//   if (document && document.body) {
-//     document.body.appendChild(app)
-//   }
-//   t.same(
-//     parse(app),
-//     strip(`
-//       <div>
-//         <div>1</div>
-//         <div>2</div>
-//       </div>
-//     `)
-//   )
-//   state.holder.current.set(state.holder.fields2)
-//   t.same(
-//     parse(app),
-//     strip(`
-//       <div>
-//         <div>3</div>
-//         <div>4</div>
-//       </div>
-//     `)
-//   )
-//   t.end()
-// })
