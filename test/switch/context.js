461e8106e4f46ab5820d122be4a1b2e1ac885072
Jim de Beer <jim@vigour.io>
2016-09-22 16:12:55 +0200

    multiple context

--- test/switch/context.js
+++ test/switch/context.js
@@ -5,11 +5,10 @@ const parse = require('parse-element')
 const strip = require('vigour-util/strip/formatting')
 const s = require('vigour-state/s')
 
-test('any - switch - type', function (t) {
+test('$switch - one level', function (t) {
   const state = s({
     holder: { val: 'text' }
   })
-
   const app = render({
     $: 'holder',
     key: 'app',
@@ -30,11 +29,6 @@ test('any - switch - type', function (t) {
       type: 'page'
     }
   }, state)
-
-  if (document && document.body) {
-    document.body.appendChild(app)
-  }
-
   t.same(
     parse(app),
     strip(`
@@ -43,8 +37,54 @@ test('any - switch - type', function (t) {
           <same>text</same>
         </switcher>
       </div>
-    `)
+    `),
+    'one level context'
   )
+  t.end()
+})
 
+test('$switch - deep', function (t) {
+  const state = s({
+    holder: { val: 'text' }
+  })
+  const app = render({
+    $: 'holder',
+    key: 'app',
+    types: {
+      page: {
+        tag: 'page',
+        switcher: {
+          tag: 'switcher',
+          $: '$switch',
+          $switch: (val) => 'sameAsAlways',
+          properties: {
+            sameAsAlways: {
+              tag: 'same',
+              text: { $: true }
+            }
+          }
+        }
+      }
+    },
+    page: {
+      type: 'page'
+    }
+  }, state)
+  t.same(
+    parse(app),
+    strip(`
+      <div>
+        <page>
+          <switcher>
+            <same>text</same>
+          </switcher>
+        </page>
+      </div>
+    `),
+    'deep context'
+  )
+  if (document.body) {
+    document.body.appendChild(app)
+  }
   t.end()
 })
