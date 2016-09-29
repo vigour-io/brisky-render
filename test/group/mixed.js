657c2e823c51eb0b5f3975682540b9843a8ece94
Jim de Beer <jim@vigour.io>
2016-09-29 15:59:06 +0200

    made test subscription leaner

--- test/group/mixed.js
+++ test/group/mixed.js
@@ -7,31 +7,44 @@ const p = require('parse-element')
 
 test('group - mixed', function (t) {
   const state = s({
-    something: { letters: { a: 'A' } }
+    something: { letters: { a: 'blue' } }
   })
+  var sub
   const app = render({
     letters: {
-      ab: {
-        $: 'something',
+      border: {
+        // $: 'something', -- default will be 1
+
+        // first for jsut travel then rest
         type: 'group',
         render: {
           state (target, s, type, stamp, subs, tree, id, pid, store) {
             const node = getParent(type, stamp, subs, tree, pid)
-            node.setAttribute('ab', `${store.a || '-'} ${store.b || '-'}`)
+            node.style.border = `${store.b} solid ${store.a}`
           }
         },
         a: {
           $: 'letters.a.$test',
           $test: state => true
         },
-        b: 'B'
+        b: '10px'
       }
     }
-  }, state)
+  }, state, (s) => {
+    sub = s
+  })
+  console.log(sub.something)
   t.equal(p(app), '<div><div ab="A B"></div></div>', 'initial')
-  state.something.letters.a.set('x')
-  t.equal(p(app), '<div><div ab="x B"></div></div>', 'update')
-  state.something.letters.a.remove()
-  t.equal(p(app), '<div><div ab="- B"></div></div>', 'remove')
+  // state.something.letters.a.set('pink')
+  // t.equal(p(app), '<div><div ab="x B"></div></div>', 'update')
+  // state.something.letters.a.remove()
+  // t.equal(p(app), '<div><div ab="- B"></div></div>', 'remove')
+
+  document.body.appendChild(app)
+
   t.end()
 })
+
+/*
+ so what to do group will need to add itself to multiple _s fields
+*/
