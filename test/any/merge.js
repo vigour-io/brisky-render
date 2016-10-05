646c4d6b6a5180bd91e68558239222f2fc8ae3dc
Jim de Beer <jim@vigour.io>
2016-10-05 15:42:15 +0200

    removed logs

--- test/any/merge.js
+++ test/any/merge.js
@@ -28,27 +28,20 @@ test('any - merge', function (t) {
     ]
   })
 
-  const elem = render(app, state, (subs) => {
-    global.s = subs
-  })
+  const elem = render(app, state)
 
-  console.info(global.s)
-
-  console.log(parse(elem))
-
-  state.collection[0].remove()
   t.equal(
     parse(elem),
     strip(`
       <div>
-        <div>
-          <span>a</span>
-          <span>b</span>
-        </div>
+        <holder>
+          <span><div>a</div></span>
+          <span><div>b</div></span>
+        </holder>
         2
       </div>
     `),
-    'remove first row'
+    'initial commit'
   )
 
   if (document.body) {
@@ -58,23 +51,23 @@ test('any - merge', function (t) {
   t.end()
 })
 
-// test('any - merge - multiple collections', function (t) {
-//   const simple = {
-//     types: {
-//       collection: {
-//         tag: 'fragment',
-//         $: 'collection.$any',
-//         child: { tag: 'b', title: { tag: 'fragment', text: { $: 'title' } } }
-//       }
-//     },
-//     holder1: { type: 'collection' },
-//     holder2: { type: 'collection' }
-//   }
-//   const app = render(simple, { collection: [ { title: 1 }, { title: 2 } ] })
-//   t.equal(
-//     parse(app),
-//     '<div><b>1</b><b>2</b><b>1</b><b>2</b></div>',
-//     'intial subscription'
-//   )
-//   t.end()
-// })
+test('any - merge - multiple collections', function (t) {
+  const simple = {
+    types: {
+      collection: {
+        tag: 'fragment',
+        $: 'collection.$any',
+        child: { tag: 'b', title: { tag: 'fragment', text: { $: 'title' } } }
+      }
+    },
+    holder1: { type: 'collection' },
+    holder2: { type: 'collection' }
+  }
+  const app = render(simple, { collection: [ { title: 1 }, { title: 2 } ] })
+  t.equal(
+    parse(app),
+    '<div><b>1</b><b>2</b><b>1</b><b>2</b></div>',
+    'intial subscription'
+  )
+  t.end()
+})
