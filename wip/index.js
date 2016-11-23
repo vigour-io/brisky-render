8a6b3af39a8a38c3c74e247ca5a664b0ec3f9945
Jim de Beer <jim@vigour.io>
2016-11-23 23:09:57 +0100

    start of subscriptions

--- wip/index.js
+++ wip/index.js
@@ -1,13 +1,21 @@
 const struct = require('brisky-struct')
 const render = require('../render')
+const state = struct({ x: 'x!' })
 
 const elem = render({
-  bla: {
-    bla: {
-      bla: true
-    },
-    text: 'hello'
-  }
-}, struct({ x: 'x!' }))
+  // bla: {
+    // bla: {
+    //   bla: {
+  text: { $: 'x' }
+    //   }
+    // },
+    // text: 'hello'
+  // }
+}, state)
 
 console.log(elem)
+document.body.appendChild(elem)
+
+state.x.set('!!!!')
+
+global.state = state
