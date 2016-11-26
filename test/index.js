107c71d4461fb4d4cc1e284c89ce5588f4459afd
Jim de Beer <jim@vigour.io>
2016-11-26 17:07:12 +0100

    enabled class tests (they fail now)

--- test/index.js
+++ test/index.js
@@ -2,31 +2,32 @@
 // require('./text')
 // require('./remove')
 
-// // // // require('./widget')
+// // // // // require('./widget')
 
 // require('./context') // needs switch
 // require('./render') // needs switch
 
-// // // require('./parent')
+// // require('./parent')
 
 // require('./html') // needs switch
 
 // require('./clone')
-// // require('./subscribe') // switch and resubscribe
+// // // require('./subscribe') // switch and resubscribe
 // require('./reference')
-// // // // require('./test')
-// // // // require('./fragment') // switch && test
-// // // // require('./sync') // not for now
+// // // // // require('./test')
+// // // // // require('./fragment') // switch && test
+// // // // // require('./sync') // not for now
 // require('./property')
 
 // require('./group')
 
 // require('./attr')
 
-// require('./class') // test
-
 // require('./any')
 
 // require('./style') // transform switch!
 
-require('./switch')
+// require('./switch')
+
+console.log('wtf wtf')
+require('./class') // test
