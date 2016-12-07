8832bb22ed9d0cf932e852cf56159c4cd11c56f2
Jim de Beer <jim@vigour.io>
2016-12-07 13:31:37 +0100

    removed logs, removed unnsecary file, enabled more tests

--- test/test/parent.js
+++ test/test/parent.js
@@ -7,142 +7,142 @@ const s = require('brisky-struct')
 const moons = require('../util/emojis').moons
 const nature = require('../util/emojis')
 
-// test('$switch - parent', t => {
-//   const state = s()
+test('$switch (test) - parent', t => {
+  const state = s()
 
-//   const emojis = {
-//     $: 'moons.$switch',
-//     $switch: {
-//       val (state, tree) {
-//         const focus = state.parent(t => t.get('focus'))
-//         return focus && focus.compute()
-//       },
-//       parent: {
-//         parent: {
-//           focus: true
-//         }
-//       }
-//     },
-//     props: {
-//       default: {
-//         tag: 'span',
-//         props: { default: 'self' }
-//       }
-//     },
-//     moon: {
-//       $: 0,
-//       text: { $: true },
-//       nature: {
-//         $: 'parent.parent.nature',
-//         text: { $: 0 }
-//       }
-//     }
-//   }
+  const emojis = {
+    $: 'moons.$switch',
+    $switch: {
+      val (state, tree) {
+        const focus = state.parent(t => t.get('focus'))
+        return focus && focus.compute()
+      },
+      parent: {
+        parent: {
+          focus: true
+        }
+      }
+    },
+    props: {
+      default: {
+        tag: 'span',
+        props: { default: 'self' }
+      }
+    },
+    moon: {
+      $: 0,
+      text: { $: true },
+      nature: {
+        $: 'parent.parent.nature',
+        text: { $: 0 }
+      }
+    }
+  }
 
-//   const types = { emojis }
+  const types = { emojis }
 
-//   const app = render({
-//     types,
-//     holder: {
-//       tag: 'holder',
-//       $: 'emojis',
-//       a: { type: 'emojis' },
-//       text: { $: 'title' },
-//       b: {
-//         type: 'emojis',
-//         $: 'deep.moons.$switch'
-//       }
-//     }
-//   }, state)
+  const app = render({
+    types,
+    holder: {
+      tag: 'holder',
+      $: 'emojis',
+      a: { type: 'emojis' },
+      text: { $: 'title' },
+      b: {
+        type: 'emojis',
+        $: 'deep.moons.$switch'
+      }
+    }
+  }, state)
 
-//   state.set({
-//     emojis: {
-//       moons: moons,
-//       nature: nature,
-//       deep: {
-//         moons: moons.concat().reverse(),
-//         nature: nature.concat().reverse()
-//       }
-//     }
-//   })
+  state.set({
+    emojis: {
+      moons: moons,
+      nature: nature,
+      deep: {
+        moons: moons.concat().reverse(),
+        nature: nature.concat().reverse()
+      }
+    }
+  })
 
-//   t.same(
-//     parse(app),
-//     '<div><holder></holder></div>',
-//     'intial render (empty)'
-//   )
+  t.same(
+    parse(app),
+    '<div><holder></holder></div>',
+    'intial render (empty)'
+  )
 
-//   // wrong order in node but not in the browser...
-//   // clone node difference
-//   state.set({ focus: true })
+  // wrong order in node but not in the browser...
+  // clone node difference
+  state.set({ focus: true })
 
-//   t.same(
-//     parse(app),
-//     strip(`
-//     <div>
-//       <holder>
-//         <div>
-//           <span>🌕<span>🐶</span></span>
-//         </div>
-//       </holder>
-//     </div>
-//     `),
-//     'set root.focus to true'
-//   )
+  t.same(
+    parse(app),
+    strip(`
+    <div>
+      <holder>
+        <div>
+          <span>🌕<span>🐶</span></span>
+        </div>
+      </holder>
+    </div>
+    `),
+    'set root.focus to true'
+  )
 
-//   state.set({ emojis: { focus: true } })
+  state.set({ emojis: { focus: true } })
 
-//   t.same(
-//     parse(app),
-//     strip(`
-//     <div>
-//       <holder>
-//         <div>
-//           <span>🌕<span>🐶</span></span>
-//         </div>
-//         <div>
-//           <span>🌔<span>💦</span></span>
-//         </div>
-//       </holder>
-//     </div>
-//     `),
-//     'set $root.emojis.focus to true'
-//   )
+  t.same(
+    parse(app),
+    strip(`
+    <div>
+      <holder>
+        <div>
+          <span>🌕<span>🐶</span></span>
+        </div>
+        <div>
+          <span>🌔<span>💦</span></span>
+        </div>
+      </holder>
+    </div>
+    `),
+    'set $root.emojis.focus to true'
+  )
 
-//   state.set({
-//     emojis: { title: '💩' }
-//   })
+  state.set({
+    emojis: { title: '💩' }
+  })
 
-//   t.same(
-//     parse(app),
-//     strip(`
-//       <div>
-//         <holder>
-//           <div>
-//             <span>🌕<span>🐶</span></span>
-//           </div>
-//           💩
-//           <div>
-//             <span>🌔<span>💦</span></span>
-//           </div>
-//         </holder>
-//       </div>
-//     `),
-//     'set emoji title'
-//   )
+  t.same(
+    parse(app),
+    strip(`
+      <div>
+        <holder>
+          <div>
+            <span>🌕<span>🐶</span></span>
+          </div>
+          💩
+          <div>
+            <span>🌔<span>💦</span></span>
+          </div>
+        </holder>
+      </div>
+    `),
+    'set emoji title'
+  )
 
-//   state.set({ focus: false, emojis: { focus: false } })
+  state.set({ focus: false, emojis: { focus: false } })
 
-//   t.same(
-//     parse(app),
-//     '<div><holder>💩</holder></div>',
-//     'set all focus fields to false'
-//   )
+  t.same(
+    parse(app),
+    '<div><holder>💩</holder></div>',
+    'set all focus fields to false'
+  )
 
-//   t.end()
-// })
+  t.end()
+})
 
-test('$switch - parent + $switch + $any', t => {
+test('$switch (test) - parent + $switch + $any', t => {
   const state = s({
     content: {
       fields: {
@@ -184,10 +184,6 @@ test('$switch - parent + $switch + $any', t => {
     }
   }
   const app = render(elem, state)
-
-  if ('body' in document) {
-    document.body.appendChild(app)
-  }
 
   t.equal(
     parse(app),
