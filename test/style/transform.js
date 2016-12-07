60533d097b16e8760334a1f6fff7085e57ae2b44
Jim de Beer <jim@vigour.io>
2016-12-07 13:56:13 +0100

    correct server side parsing of transform

--- test/style/transform.js
+++ test/style/transform.js
@@ -35,30 +35,30 @@ test('transform - state', (t) => {
   t.end()
 })
 
-// test('transform - state - remove', t => {
-//   const state = s({
-//     a: {
-//       y: 5
-//     },
-//     b: {},
-//     field: '$root.a'
-//   })
-//   const elem = render({
-//     field: {
-//       $: 'field.$switch',
-//       properties: {
-//         a: {
-//           style: {
-//             transform: {
-//               y: { $: 'y' }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }, state)
-//   t.equals(elem.childNodes[0].childNodes[0].style.transform, 'translate3d(0, 5px, 0px)', 'intial')
-//   state.set({ field: '$root.b' })
-//   t.ok(true, 'should not crash on remove')
-//   t.end()
-// })
+test('transform - state - remove', t => {
+  const state = s({
+    a: {
+      y: 5
+    },
+    b: {},
+    field: [ '@', 'root', 'a' ]
+  })
+  const elem = render({
+    field: {
+      $: 'field.$switch',
+      props: {
+        a: {
+          style: {
+            transform: {
+              y: { $: 'y' }
+            }
+          }
+        }
+      }
+    }
+  }, state)
+  t.equals(elem.childNodes[0].style.transform, 'translate3d(0px, 5px, 0px)', 'intial')
+  state.set({ field: [ '@', 'root', 'b' ] })
+  t.ok(true, 'should not crash on remove')
+  t.end()
+})
