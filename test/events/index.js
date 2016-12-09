a05c1915a28ed02110939ffc6ffd65ddaeb37321
Jim de Beer <jim@vigour.io>
2016-12-09 19:48:51 +0100

    add more event tests

--- test/events/index.js
+++ test/events/index.js
@@ -1,12 +1,14 @@
 const render = require('../../render')
 const test = require('tape')
+const struct = require('brisky-struct')
 
 test('events - basic', t => {
   const app = render({
     thing: {
       text: 'thing',
       style: {
-        fontSize: '200px'
+        fontSize: '100px',
+        fontFamily: 'SF Ui Display'
       },
       on: {
         click (e) {
@@ -18,5 +20,41 @@ test('events - basic', t => {
   if (document.body) {
     document.body.appendChild(app)
   }
+  t.end()
+})
+
+test('events - context on state', t => {
+  const state = struct({
+    types: {
+      ha: { x: 'ha!' },
+      greeting: { type: 'ha' }
+    },
+    bla: {
+      hello: {
+        type: 'greeting'
+      }
+    }
+  })
+
+  const app = render({
+    thing: {
+      text: { $: true },
+      style: {
+        fontSize: '200px',
+        fontFamily: 'SF Ui Display'
+      },
+      $: 'bla.hello.x',
+      on: {
+        click (e) {
+          console.log('clikerdiclickclick', e, e.state)
+        }
+      }
+    }
+  }, state)
+
+  if (document.body) {
+    document.body.appendChild(app)
+  }
+
   t.end()
 })
