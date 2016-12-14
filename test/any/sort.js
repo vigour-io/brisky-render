abc918c5cbf92d54138b5ad9a4e38521dbbfb98c
Jim de Beer <jim@vigour.io>
2016-12-14 21:59:12 +0100

    added sort tests and nested subs

--- test/any/sort.js
+++ test/any/sort.js
@@ -1,12 +1,21 @@
 const render = require('../../render')
 const test = require('tape')
-// const parse = require('parse-element')
-// const strip = require('strip-formatting')
+const parse = require('parse-element')
+const strip = require('strip-formatting')
 const s = require('brisky-struct')
 
 test('any - sort', t => {
   const app = {
     types: {
+      special: {
+        type: 'style',
+        fontSize: '40px',
+        textAlign: 'center',
+        padding: '5px',
+        background: '#333',
+        color: '#eee',
+        opacity: 1
+      },
       holder: {
         style: {
           borderTop: '1px solid #333',
@@ -20,46 +29,93 @@ test('any - sort', t => {
         props: {
           default: {
             style: {
+              type: 'special',
               display: 'inline-block',
-              fontSize: '40px',
               borderRadius: '50%',
               margin: '15px',
-              textAlign: 'center',
-              padding: '5px',
-              background: '#333',
               width: '50px',
               height: '50px',
-              color: '#eee'
+              color: { $: 'color' },
+              something: { $: 'blurf' },
+              fontWeight: { $: 'styles.weight' },
+              transition: 'transform 0.5s',
+              transform: {
+                scale: { $: true, $transform: val => (5 + val) / 10 }
+              }
             },
-            text: { $: true }
+            on: {
+              remove: ({ target }) => {
+                const fade = () => {
+                  target.style.opacity = target.style.opacity - 0.001
+                  if (target.style.opacity > 0) {
+                    setTimeout(fade, 18)
+                  } else if (target.parentNode) {
+                    target.parentNode.removeChild(target)
+                  }
+                }
+                fade()
+              }
+            },
+            text: { $: true },
+            text2: { type: 'text', $: 'root.james' }
           }
         }
       }
     },
-    a: { type: 'holder' },
-    b: { type: 'holder' },
+    props: {
+      default: {
+        type: 'holder'
+      }
+    },
+    a: {},
+    b: {},
     c: {
-      type: 'holder',
-      // need to subscribe in an object to make this work...
       $any: (keys, state) => keys.filter(val => state.get(val).compute() > 4)
+    },
+    hello: {
+      type: 'element',
+      hello: {
+        $: 'query',
+        style: {
+          type: 'special'
+        },
+        text: { $: 'root.james' }
+      }
+    },
+    d: {
+      $any: {
+        val: (keys, state) => keys.filter(
+          val => state.root().query.compute() === state[val].compute() ||
+            state.root().james.compute() === state[val].compute()
+        ),
+        root: {
+          query: true,
+          james: true
+        },
+        color: {
+          val: true,
+          smurf: true
+        },
+        blurf: 1,
+        styles: { val: true }
+        // same as well -- need to test
+      }
     }
   }
 
   const state = s({
-    collection: [ 1, 2, 3, 4, 5 ]
+    collection: [ 1, 2, 3, 4, 5 ],
+    query: 1,
+    james: '!'
   })
 
-  const elem = render(app, state, (s, tree, app, ss, type) => {
-    if (ss) {
-      console.warn(ss.path(), type)
-    }
-  })
+  state.collection.set(state.collection.map(val => ({ val: val.compute(), color: 'rgb(' + val.compute() * 20 + ', 100, 100)' })))
 
-  console.log('go go go')
+  const elem = render(app, state)
 
   var cnt = 0
   const defer = (val) => new Promise(resolve => {
-    setTimeout(() => resolve(val), (++cnt) * 500)
+    setTimeout(() => resolve(val), (++cnt) * 2)
   })
 
   state.collection.set(defer({ 0: 6 }))
@@ -67,22 +123,21 @@ test('any - sort', t => {
   state.collection.set(defer({ 4: 1 }))
   state.collection.set(defer({ 0: 1 }))
 
-  // t.equal(
-  //   parse(elem),
-  //   strip(`
-  //     <div>
-  //       <holder>
-  //         <span><div>a</div></span>
-  //         <span><div>b</div></span>
-  //       </holder>
-  //       2
-  //     </div>
-  //   `)
-  // )
+  state.query.set(defer(7))
 
   if (document.body) {
     document.body.appendChild(elem)
   }
 
-  t.end()
+  const result = document.body
+    ? strip(`<div><div style="border-top: 1px solid rgb(51, 51, 51); padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">7!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(60, 100, 100); transform: scale(0.8);">3!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(40, 100, 100); transform: scale(0.7);">2!</div></div><div style="border-top: 1px solid rgb(51, 51, 51); padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">7!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(60, 100, 100); transform: scale(0.8);">3!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(40, 100, 100); transform: scale(0.7);">2!</div></div><div style="border-top: 1px solid rgb(51, 51, 51); padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">7!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 0.994; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 0.994; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(100, 100, 100); transform: scale(1);">!</div></div><div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); color: rgb(238, 238, 238); opacity: 1;">!</div></div><div style="border-top: 1px solid rgb(51, 51, 51); padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 0.994; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(20, 100, 100); transform: scale(0.6);">!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(20, 100, 100); transform: scale(0.6);">1!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: rgb(51, 51, 51); opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(100, 100, 100); transform: scale(0.6);">1!</div></div></div>`)
+    : strip(`<div><div style="border-top: 1px solid #333; padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">7!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(60, 100, 100); transform: scale(0.8);">3!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(40, 100, 100); transform: scale(0.7);">2!</div></div><div style="border-top: 1px solid #333; padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">7!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(60, 100, 100); transform: scale(0.8);">3!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(40, 100, 100); transform: scale(0.7);">2!</div></div><div style="border-top: 1px solid #333; padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">7!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 0.994; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(80, 100, 100); transform: scale(1.2);">!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 0.994; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(100, 100, 100); transform: scale(1);">!</div></div><div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; color: #eee; opacity: 1;">!</div></div><div style="border-top: 1px solid #333; padding: 50px; text-align: center;"><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 0.994; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(20, 100, 100); transform: scale(0.6);">!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(20, 100, 100); transform: scale(0.6);">1!</div><div style="font-size: 40px; text-align: center; padding: 5px; background: #333; opacity: 1; display: inline-block; border-radius: 50%; margin: 15px; width: 50px; height: 50px; transition: transform 0.5s; color: rgb(100, 100, 100); transform: scale(0.6);">1!</div></div></div>`)
+
+  state.query.once(7, () => {
+    t.equal(
+      parse(elem),
+      result
+    )
+    t.end()
+  })
 })
