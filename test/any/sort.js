6553f0254916dbc28fb9ed2b53f862756ea7761e
Jim de Beer <jim@vigour.io>
2016-12-14 19:09:53 +0100

    sort example

--- test/any/sort.js
+++ test/any/sort.js
@@ -8,11 +8,15 @@ test('any - sort', t => {
   const app = {
     types: {
       holder: {
+        style: {
+          padding: '20px',
+          fontSize: '40px'
+        },
         tag: 'holder',
         $: 'collection.$any',
-        $any: (keys, state) => {
-          return keys.filter(val => val > 2)
-        },
+        $any: (keys, state) => keys.concat().sort(
+          (a, b) => state.get(a).compute() > state.get(b).compute() ? -1 : 1
+        ),
         props: {
           default: {
             title: { text: { $: true } }
@@ -21,16 +25,26 @@ test('any - sort', t => {
       }
     },
     a: { type: 'holder' },
-    b: { type: 'holder' }
+    b: { type: 'holder' },
+    c: {
+      type: 'holder',
+      // need to subscribe in an object to make this work...
+      $any: (keys, state) => keys.filter(val => state.get(val).compute() > 4)
+    }
   }
 
   const state = s({
     collection: [ 1, 2, 3, 4, 5 ]
   })
 
-  const elem = render(app, state, (s, tree) => {
-    console.warn(s)
+  const elem = render(app, state, (s, tree, app, ss, type) => {
+    if (ss) {
+      console.warn(ss.path(), type)
+    }
   })
+
+  console.log('go go go')
+  state.collection[0].set(6)
 
   // t.equal(
   //   parse(elem),
