443c1fce5cc5f5837e3fa789df5f089de1e9e927
Youri Daamen <me@youz.io>
2016-12-20 12:24:17 +0100

    added rollup folders

---
+++ wip/rollup/config.js
@@ -0,0 +1,27 @@
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const envs = require('rollup-plugin-envs')
+
+module.exports = [{
+  entry: './index.js',
+  plugins: [
+    {
+      load (id) {
+        console.log(id)
+      }
+    },
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs()//,
+    // envs({
+    //   import: [ 'vigour-ua/navigator' ]
+    // })
+  ],
+  dest: 'dist/index.browser.dev.js',
+  format: 'iife',
+  moduleName: 'wip',
+  intro: 'var global = window;'
+}]
