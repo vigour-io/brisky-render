94b351027f7786d16c87b938a83219cab3c404b6
Jim de Beer <jim@vigour.io>
2016-12-23 18:03:16 +0100

    correct deps

--- wip/server/browser.js
+++
@@ -1,5 +0,0 @@
-import { body } from '../src/index.js'
-import state from '../src/state.js'
-import render from './render.js'
-
-render(body, state)
--- wip/src/state.js
+++
@@ -1,8 +0,0 @@
-import hub from 'hub.js'
-
-const x = global.state = hub({
-  // id: 'GURRR',
-  url: 'ws://localhost:3031'
-})
-
-export default x
