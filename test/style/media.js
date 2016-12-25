d6829e94765b8997d4166947bab5299a6e5ae541
Jim de Beer <jim@vigour.io>
2016-12-25 14:24:30 +0100

    counter till 1k individual styles

--- test/style/media.js
+++ test/style/media.js
@@ -8,7 +8,21 @@ test('style - media styletron with classnames', t => {
     class: 'extra'
   })
 
+  var i = 1000
+  var arr = []
+  while (i--) {
+    arr.push({
+      text: i,
+      style: {
+        border: '1px solid red',
+        backgroundColor: 'pink',
+        padding: `${i}px`
+      }
+    })
+  }
+
   const elem = render({
+    vibes: arr,
     x: {
       style: {
         padding: '10px',
@@ -18,13 +32,23 @@ test('style - media styletron with classnames', t => {
         val: 'hello',
         extra: { $: 'class' }
       },
-
+      text: 'hello'
+    },
+    y: {
+      style: {
+        padding: '10px',
+        opacity: 0.5
+      },
+      class: {
+        val: 'hello',
+        extra: { $: 'class' }
+      },
       text: 'hello'
     }
   }, state)
-  if (document.body) {
-    document.body.appendChild(elem)
-  }
+
+  if (document.body) document.body.appendChild(elem)
+
   t.equal(elem.className, 'hello extra a b')
   state.class.set('gurt')
   t.equal(elem.className, 'hello gurt a b')
