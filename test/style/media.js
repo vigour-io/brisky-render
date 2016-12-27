3792de6ed9d6226e0cb64e999934ff78c9df01fd
Jim de Beer <jim@vigour.io>
2016-12-27 13:33:27 +0100

    correct media queries

--- test/style/media.js
+++ test/style/media.js
@@ -2,6 +2,7 @@ const test = require('tape')
 const { create } = require('brisky-struct')
 const { render, clearStyleCache } = require('../../')
 const parse = require('parse-element')
+const strip = require('strip-formatting')
 
 test('style - media styletron with classnames', t => {
   clearStyleCache()
@@ -42,9 +43,9 @@ test('style - media styletron with classnames', t => {
 
   if (document.body) document.body.appendChild(elem)
 
-  t.equal(elem.className, 'hello extra a b c _d')
+  t.equal(elem.className, 'hello extra a b c a1')
   state.class.set('gurt')
-  t.equal(elem.className, 'hello gurt a b c _d')
+  t.equal(elem.className, 'hello gurt a b c a1')
   t.end()
 })
 
@@ -77,7 +78,35 @@ test('style - multiple media queries', t => {
 
   if (document.body) document.body.appendChild(elem)
 
-  t.equal(parse(elem), '<div><div class=" a _b">a</div><div class=" c d _e _a">b</div><style> .a {background:yellow;} .b {background:red;} .c {background:grey;} .d {min-width:50%;} .e {background:blue;}  @media (min-width: 700px) { ._b {background:red;} ._a {background:yellow;} } @media (min-width: 480px) { ._e {background:blue;} } </style></div>')
+  t.equal(parse(elem), strip(`<div><div class=" a a1">a</div><div class=" b c a2 b1">b</div><style> .a {background:yellow;} .b {background:grey;} .c {min-width:50%;}  @media (min-width: 700px) { .a1 {background:red;} .b1 {background:yellow;} } @media (min-width: 480px) { .a2 {background:blue;} } </style></div>`))
+
+  t.end()
+})
+
+test('style - a should not be blue', t => {
+  clearStyleCache()
+  const elem = render({
+    a: {
+      style: {
+        [`@media (min-width: 10000px)`]: {
+          background: 'blue'
+        }
+      },
+      text: 'a'
+    },
+    b: {
+      style: {
+        [`@media (min-width: 100px)`]: {
+          background: 'blue'
+        }
+      },
+      text: 'b'
+    }
+  })
+
+  if (document.body) document.body.appendChild(elem)
+
+  t.equal(parse(elem), `<div><div class=" a1">a</div><div class=" a2">b</div><style>  @media (min-width: 10000px) { .a1 {background:blue;} } @media (min-width: 100px) { .a2 {background:blue;} } </style></div>`)
 
   t.end()
 })
