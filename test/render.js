77217b28387748b695bb1959c0620391bac15d8a
Jim de Beer <jim@vigour.io>
2017-01-18 22:17:30 +0100

    first dom renders

--- test/render.js
+++ test/render.js
@@ -3,47 +3,90 @@ const test = require('tape')
 const { create: s } = require('brisky-struct')
 const p = require('parse-element')
 
-test('render - $any on top', t => {
-  const state = s([ 1, 2 ])
-  const app = render(
-    {
-      $: '$any',
-      tag: 'ul',
-      props: {
-        default: {
-          tag: 'li',
-          text: { $: true }
-        }
-      }
-    },
-    state
-  )
-  t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
-  t.end()
-})
+// test('render - $any on top', t => {
+//   const state = s([ 1, 2 ])
+//   const app = render(
+//     {
+//       $: '$any',
+//       tag: 'ul',
+//       props: {
+//         default: {
+//           tag: 'li',
+//           text: { $: true }
+//         }
+//       }
+//     },
+//     state
+//   )
+//   t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
+//   t.end()
+// })
 
-test('render - $switch on top', t => {
+// test('render - $switch on top', t => {
+//   const state = s({
+//     text: ':/',
+//     html: ':)'
+//   })
+//   const app = render(
+//     {
+//       switcher: {
+//         $: 'a.$switch',
+//         $switch: state => state.origin().key,
+//         props: {
+//           text: { type: 'text', $: true },
+//           html: { type: 'element', html: { $: true } }
+//         }
+//       }
+//     },
+//     state
+//   )
+//   t.equal(p(app), '<div></div>', 'initial')
+//   state.set({ a: state.text })
+//   t.equal(p(app), '<div>:/</div>', 'swtich')
+//   state.a.set(state.html)
+//   t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
+//   t.end()
+// })
+
+test('render - to element', t => {
   const state = s({
-    text: ':/',
-    html: ':)'
+    a: '☄️',
+    b: '🐗'
   })
+
+  console.log(document.documentElement)
+
+  var x = document.documentElement
+
   const app = render(
+    x,
     {
-      switcher: {
-        $: 'a.$switch',
-        $switch: state => state.origin().key,
-        props: {
-          text: { type: 'text', $: true },
-          html: { type: 'element', html: { $: true } }
-        }
+      text: '???',
+      head: {
+        title: { tag: 'title', text: 'yo!' } // make some default
+      },
+      body: {
+        tag: 'body',
+        text: 'x'
       }
+      // switcher: {
+      //   $: 'a.$switch',
+      //   $switch: state => state.origin().key,
+      //   props: {
+      //     text: { type: 'text', $: true },
+      //     html: { type: 'element', html: { $: true } }
+      //   }
+      // }
     },
     state
   )
-  t.equal(p(app), '<div></div>', 'initial')
-  state.set({ a: state.text })
-  t.equal(p(app), '<div>:/</div>', 'swtich')
-  state.a.set(state.html)
-  t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
+
+  console.log(x, app)
+  console.log(app === x)
+  // t.equal(p(app), '<div></div>', 'initial')
+  // state.set({ a: state.text })
+  // t.equal(p(app), '<div>:/</div>', 'swtich')
+  // state.a.set(state.html)
+  // t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
   t.end()
 })
