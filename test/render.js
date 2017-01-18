9011b02d1979b693b3277ad8b47f23887b57e8ba
Jim de Beer <jim@vigour.io>
2017-01-18 22:55:48 +0100

    nice example

--- test/render.js
+++ test/render.js
@@ -51,35 +51,79 @@ const p = require('parse-element')
 test('render - to element', t => {
   const state = s({
     a: '☄️',
-    b: '🐗'
+    b: '🐗',
+    loader: 1
   })
+
+  var dir = 1
+  setInterval(() => {
+    state.loader.set(state.loader.compute() + dir)
+  }, 18)
 
   console.log(document.documentElement)
 
   var x = document.documentElement
 
+  // make an iframe
+
+  /*
+    <!-- Suppress browser request for favicon.ico -->
+    <link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,">
+  */
+
+  const canvas = document.createElement('canvas')
+  canvas.width = 32
+  canvas.height = 32
+  const context = canvas.getContext('2d')
+  context.fillStyle = 'green'
+
+
   const app = render(
-    x,
+    document.documentElement,
     {
-      text: '???',
       head: {
-        title: { tag: 'title', text: 'yo!' } // make some default
+
+        favicon: {
+          tag: 'link',
+          attr: {
+            rel: 'shortcut icon',
+            href: {
+              $: 'loader',
+              $transform: val => {
+                context.clearRect(0, 0, 32, 32)
+                let i = 52
+                while (i--) {
+                  context.fillStyle = `rgb(${7.5 * i / 3},${7.5 * i},${7.5 * i})`
+                  context.fillRect(
+                    Math.sin(val / 100 + i / 7) * 15 + 15,
+                    Math.cos(val / 100 + i / 7) * 15 + 15, 2, 2
+                  )
+                }
+                // let i = 32
+                // while (i--) {
+                //   let j = 32
+                //   while (j--) {
+                //     if (val < 200) {
+                //       context.fillStyle = `rgb(${val + i},${val + j},${val + (i + j)})`
+                //       context.fillRect(i, j, 1, 1)
+                //     }
+                //   }
+                // }
+                return canvas.toDataURL('image/png')
+              }
+            }
+          }
+        },
+        title: { tag: 'title', text: { $: 'loader' } } // make some default
       },
       body: {
         tag: 'body',
         text: 'x'
       }
-      // switcher: {
-      //   $: 'a.$switch',
-      //   $switch: state => state.origin().key,
-      //   props: {
-      //     text: { type: 'text', $: true },
-      //     html: { type: 'element', html: { $: true } }
-      //   }
-      // }
     },
     state
   )
+  document.body.appendChild(canvas)
 
   console.log(x, app)
   console.log(app === x)
