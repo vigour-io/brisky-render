a422a6b7da75ecc4ebf07720396a6635c3de43f4
Jim de Beer <jim@vigour.io>
2017-01-19 10:51:39 +0100

    added test

--- test/render.js
+++ test/render.js
@@ -2,83 +2,64 @@ const { render } = require('../')
 const test = require('tape')
 const { create: s } = require('brisky-struct')
 const p = require('parse-element')
+const strip = require('strip-formatting')
 
-// test('render - $any on top', t => {
-//   const state = s([ 1, 2 ])
-//   const app = render(
-//     {
-//       $: '$any',
-//       tag: 'ul',
-//       props: {
-//         default: {
-//           tag: 'li',
-//           text: { $: true }
-//         }
-//       }
-//     },
-//     state
-//   )
-//   t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
-//   t.end()
-// })
+test('render - $any on top', t => {
+  const state = s([ 1, 2 ])
+  const app = render(
+    {
+      $: '$any',
+      tag: 'ul',
+      props: {
+        default: {
+          tag: 'li',
+          text: { $: true }
+        }
+      }
+    },
+    state
+  )
+  t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
+  t.end()
+})
 
-// test('render - $switch on top', t => {
-//   const state = s({
-//     text: ':/',
-//     html: ':)'
-//   })
-//   const app = render(
-//     {
-//       switcher: {
-//         $: 'a.$switch',
-//         $switch: state => state.origin().key,
-//         props: {
-//           text: { type: 'text', $: true },
-//           html: { type: 'element', html: { $: true } }
-//         }
-//       }
-//     },
-//     state
-//   )
-//   t.equal(p(app), '<div></div>', 'initial')
-//   state.set({ a: state.text })
-//   t.equal(p(app), '<div>:/</div>', 'swtich')
-//   state.a.set(state.html)
-//   t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
-//   t.end()
-// })
+test('render - $switch on top', t => {
+  const state = s({
+    text: ':/',
+    html: ':)'
+  })
+  const app = render(
+    {
+      switcher: {
+        $: 'a.$switch',
+        $switch: state => state.origin().key,
+        props: {
+          text: { type: 'text', $: true },
+          html: { type: 'element', html: { $: true } }
+        }
+      }
+    },
+    state
+  )
+  t.equal(p(app), '<div></div>', 'initial')
+  state.set({ a: state.text })
+  t.equal(p(app), '<div>:/</div>', 'swtich')
+  state.a.set(state.html)
+  t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
+  t.end()
+})
 
 test('render - to element', t => {
-  const state = s({
-    a: '☄️',
-    b: '🐗',
-    loader: 1
-  })
+  const state = s({ loader: 1 })
 
-  var dir = 1
-  setInterval(() => {
-    state.loader.set(state.loader.compute() + dir)
-  }, 18)
+  const strange = document.createElement('strange')
 
-  console.log(document.documentElement)
-
-  var x = document.documentElement
-
-  // make an iframe
-
-  /*
-    <!-- Suppress browser request for favicon.ico -->
-    <link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,">
-  */
-
-  const canvas = document.createElement('canvas')
-  canvas.width = 32
-  canvas.height = 32
-  const context = canvas.getContext('2d')
-  context.fillStyle = 'green'
+  const head = document.createElement('head')
+  head.innerHTML = 'blurf'
+  strange.appendChild(head)
 
   const app = render(
-    document.documentElement,
+    strange,
     {
       head: {
         tag: 'head',
@@ -88,34 +69,7 @@ test('render - to element', t => {
             rel: 'shortcut icon',
             href: {
               $: 'loader',
-              $transform: val => {
-                context.clearRect(0, 0, 32, 32)
-                context.beginPath()
-                context.arc(16, 16, 13, val / 50, 0, false)
-                context.fillStyle = 'green'
-                context.lineWidth = 3
-                context.strokeStyle = '#003300'
-                context.stroke()
-                // let i = 52
-                // while (i--) {
-                //   context.fillStyle = `rgb(${7.5 * i / 3},${7.5 * i},${7.5 * i})`
-                //   context.fillRect(
-                //     Math.sin(val / 100 + i / 7) * 15 + 15,
-                //     Math.cos(val / 100 + i / 7) * 15 + 15, 1, 1
-                //   )
-                // }
-                // let i = 32
-                // while (i--) {
-                //   let j = 32
-                //   while (j--) {
-                //     if (val < 200) {
-                //       context.fillStyle = `rgb(${val + i},${val + j},${val + (i + j)})`
-                //       context.fillRect(i, j, 1, 1)
-                //     }
-                //   }
-                // }
-                return canvas.toDataURL('image/png')
-              }
+              $transform: val => `${val}.jpg`
             }
           }
         },
@@ -128,10 +82,20 @@ test('render - to element', t => {
     },
     state
   )
-  document.body.appendChild(canvas)
 
-  console.log(x, app)
-  console.log(app === x)
+  t.equal(app, strange, 'enahnces original')
+
+  // overwrites existing (this is debatable)
+  t.equal(p(app), strip(`
+    <strange>
+       <head>
+          <link rel="shortcut icon" href="1.jpg">
+          <title>1</title>
+       </head>
+       <body>x</body>
+    </strange>
+  `))
+
   // t.equal(p(app), '<div></div>', 'initial')
   // state.set({ a: state.text })
   // t.equal(p(app), '<div>:/</div>', 'swtich')
