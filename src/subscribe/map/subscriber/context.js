e4c6953d75b04cb74ab2ee90e2dd683c1cd01a9b
Jim de Beer <jim@vigour.io>
2017-01-26 12:52:52 +0100

    use cuid

--- src/subscribe/map/subscriber/context.js
+++ src/subscribe/map/subscriber/context.js
@@ -1,3 +1,5 @@
+import { cuid } from 'brisky-struct'
+
 const contextKey = t => t.storeContextKey !== void 0
   ? t.storeContextKey
   : t.inherits && contextKey(t.inherits)
@@ -5,21 +7,8 @@ const contextKey = t => t.storeContextKey !== void 0
 export default t => {
   if (contextKey(t)) {
     const key = t.parent().key
-    return key ? 'c' + key + '-' + genCid(t) : 'c' + genCid(t)
+    return key ? 'c' + key + '-' + cuid(t) : 'c' + cuid(t)
   } else {
-    return 'c' + genCid(t)
-  }
-}
-
-// this will become a method in struct
-const genCid = t => {
-  if (t._c) {
-    if (t._cLevel === 1) {
-      return (t.uid() - 1e4) + '' + genCid(t._c) // wrong
-    } else {
-      return (t.uid() - 1e4) + '' + genCid(t._p)
-    }
-  } else {
-    return t.uid() - 1e4
+    return 'c' + cuid(t)
   }
 }
