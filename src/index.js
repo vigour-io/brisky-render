d38c1dbd76fa470c4a0a3a2319324b0d5d81086a
Youri Daamen <me@youz.io>
2017-02-08 18:37:08 +0100

    dirname check

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import parent from './render/dom/parent'
 import attr from './property/attr'
 import { clear as clearStyleCache } from './property/style/sheet'
 
-console.log('brisky-render:', __dirname)
+if (typeof __dirname !== 'undefined') console.log('hub.js:', __dirname)
 
 export { render, element, parent, attr, clearStyleCache }
