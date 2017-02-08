acf188885e0f575bec8ca766696009a17e74e4a4
Youri Daamen <me@youz.io>
2017-02-08 18:29:18 +0100

    log

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import parent from './render/dom/parent'
 import attr from './property/attr'
 import { clear as clearStyleCache } from './property/style/sheet'
 
+console.log('brisky-render:', __dirname)
+
 export { render, element, parent, attr, clearStyleCache }
