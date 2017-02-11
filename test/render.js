820b8c5f120d8ea146cf9848f30376998eb9b9da
Youri Daamen <me@youz.io>
2017-02-11 13:03:50 +0100

    fixed tests

--- test/render.js
+++ test/render.js
@@ -5,50 +5,50 @@ const p = require('parse-element')
 const strip = require('strip-formatting')
 // stirp formatting will remove data-hash
 
-// test('render - $any on top', t => {
-//   const state = s([ 1, 2 ])
-//   const app = render(
-//     {
-//       $: '$any',
-//       tag: 'ul',
-//       props: {
-//         default: {
-//           tag: 'li',
-//           text: { $: true }
-//         }
-//       }
-//     },
-//     state
-//   )
-//   t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
-//   t.end()
-// })
+test('render - $any on top', t => {
+  const state = s([ 1, 2 ])
+  const app = render(
+    {
+      $: '$any',
+      tag: 'ul',
+      props: {
+        default: {
+          tag: 'li',
+          text: { $: true }
+        }
+      }
+    },
+    state
+  )
+  t.equal(p(app), '<ul><li>1</li><li>2</li></ul>', 'initial')
+  t.end()
+})
 
-// test('render - $switch on top', t => {
-//   const state = s({
-//     text: ':/',
-//     html: ':)'
-//   })
-//   const app = render(
-//     {
-//       switcher: {
-//         $: 'a.$switch',
-//         $switch: state => state.origin().key,
-//         props: {
-//           text: { type: 'text', $: true },
-//           html: { type: 'element', html: { $: true } }
-//         }
-//       }
-//     },
-//     state
-//   )
-//   t.equal(p(app), '<div></div>', 'initial')
-//   state.set({ a: state.text })
-//   t.equal(p(app), '<div>:/</div>', 'swtich')
-//   state.a.set(state.html)
-//   t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
-//   t.end()
-// })
+test('render - $switch on top', t => {
+  const state = s({
+    text: ':/',
+    html: ':)'
+  })
+  const app = render(
+    {
+      switcher: {
+        $: 'a.$switch',
+        $switch: state => state.origin().key,
+        props: {
+          text: { type: 'text', $: true },
+          html: { type: 'element', html: { $: true } }
+        }
+      }
+    },
+    state
+  )
+  t.equal(p(app), '<div></div>', 'initial')
+  state.set({ a: state.text })
+  t.equal(p(app), '<div>:/</div>', 'swtich')
+  state.a.set(state.html)
+  t.equal(p(app), '<div><div>:)</div></div>', 'change reference')
+  t.end()
+})
 
 test('render - to element', t => {
   element.noResolve(false)
