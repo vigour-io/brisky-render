fadabe8b2a9485c354eed6be0b275f3d7407d3a5
Jim de Beer <jim@vigour.io>
2017-02-27 19:03:16 +0100

    re-enable test

--- test/subscribe.js
+++ test/subscribe.js
@@ -3,22 +3,22 @@ const test = require('tape')
 const { create: s } = require('brisky-struct')
 const p = require('parse-element')
 
-// test('subscribe - merge', t => {
-//   const state = s({
-//     field: 'its text'
-//   })
-//   const app = render(
-//     {
-//       text: { $: 'field' },
-//       a: { tag: 'a', $: 'field' }
-//     },
-//     state
-//   )
-//   t.equal(p(app), '<div>its text<a></a></div>', 'initial')
-//   state.field.set('update')
-//   t.equal(p(app), '<div>update<a></a></div>', 'fires update')
-//   t.end()
-// })
+test('subscribe - merge', t => {
+  const state = s({
+    field: 'its text'
+  })
+  const app = render(
+    {
+      text: { $: 'field' },
+      a: { tag: 'a', $: 'field' }
+    },
+    state
+  )
+  t.equal(p(app), '<div>its text<a></a></div>', 'initial')
+  state.field.set('update')
+  t.equal(p(app), '<div>update<a></a></div>', 'fires update')
+  t.end()
+})
 
 test('subscribe - object subscription', t => {
   const state = s({
