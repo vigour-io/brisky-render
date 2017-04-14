db9e9a1acb1e82aecd78a2013df631944e236b7b
Jim de Beer <jim@vigour.io>
2017-04-14 12:22:24 +0200

    comments

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ import attr from './property/attr'
 import prefix from './property/style/prefix'
 import './size'
 
+/*
+window.getDevicePixelRatio = function () {
+    var ratio = 1;
+    // To account for zoom, change to use deviceXDPI instead of systemXDPI
+    if (window.screen.systemXDPI !== undefined && window.screen.logicalXDPI       !== undefined && window.screen.systemXDPI > window.screen.logicalXDPI) {
+        // Only allow for values > 1
+        ratio = window.screen.systemXDPI / window.screen.logicalXDPI;
+    }
+    else if (window.devicePixelRatio !== undefined) {
+        ratio = window.devicePixelRatio;
+    }
+    return ratio;
+}
+*/
+
 if (!global.devicePixelRatio) {
   global.devicePixelRatio = 1
 }
