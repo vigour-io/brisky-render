160d875a42c4254d55e1ccece028888fa67cdb25
Jim de Beer <jim@vigour.io>
2017-05-30 18:13:18 +0200

    remove double events

--- src/events/index.js
+++ src/events/index.js
@@ -6,12 +6,13 @@ import listen from './listener'
 const emitterProperty = struct.props.on.struct.props.default
 const cache = {}
 const injectable = {}
-// const isTouch = typeof window !== 'undefined' && (
-//   ('ontouchstart' in global ||
-//     global.DocumentTouch &&
-//     document instanceof global.DocumentTouch) ||
-//   navigator.msMaxTouchPoints ||
-//   false)
+
+const isTouch = typeof window !== 'undefined' && (
+  ('ontouchstart' in global ||
+    global.DocumentTouch &&
+    document instanceof global.DocumentTouch) ||
+  navigator.msMaxTouchPoints ||
+  false)
 
 const clear = () => { block = null }
 const blockMouse = () => {
@@ -55,45 +56,50 @@ injectable.on = {
     down: (t, val, key) => {
       if (!cache[key]) {
         cache[key] = true
-        listen('mousedown', e => {
-          blockClick = Date.now()
-          if (!block) delegate(key, e)
-        })
-        listen('touchstart', e => {
-          blockClick = Date.now()
-          blockMouse()
-          delegate(key, e)
-        })
-      }
-      t._p.set({ hasEvents: true }, false)
-      emitterProperty(t, val, key)
-    },
-    up: (t, val, key) => {
-      if (!cache[key]) {
-        cache[key] = true
-        listen('mouseup', e => {
-          !block && delegate(key, e)
-        })
-        listen('touchend', e => {
-          blockMouse()
-          delegate(key, e)
-        })
+        if (!isTouch) {
+          listen('mousedown', e => {
+            blockClick = Date.now()
+            // if (!block) delegate(key, e)
+            delegate(key, e)
+          })
+        } else {
+          listen('touchstart', e => {
+            blockClick = Date.now()
+            // blockMouse()
+            delegate(key, e)
+          })
+        }
       }
       t._p.set({ hasEvents: true }, false)
       emitterProperty(t, val, key)
     }
+    // up: (t, val, key) => {
+    //   if (!cache[key]) {
+    //     cache[key] = true
+    //     listen('mouseup', e => {
+    //       delegate(key, e)
+    //       // !block && delegate(key, e)
+    //     })
+    //     listen('touchend', e => {
+    //       // blockMouse()
+    //       delegate(key, e)
+    //     })
+    //   }
+    //   t._p.set({ hasEvents: true }, false)
+    //   emitterProperty(t, val, key)
+    // }
   }
 }
 
-// if (isTouch) {
-//   injectable.on.props.move = (t, val) => t.set({ touchmove: val })
-//   injectable.on.props.down = (t, val) => t.set({ touchstart: val })
-//   injectable.on.props.up = (t, val) => t.set({ touchend: val })
-// } else {
-//   injectable.on.props.move = (t, val) => t.set({ mousemove: val })
-//   injectable.on.props.down = (t, val) => t.set({ mousedown: val })
-//   injectable.on.props.up = (t, val) => t.set({ mouseup: val })
-// }
+if (isTouch) {
+  injectable.on.props.move = (t, val) => t.set({ touchmove: val })
+  // injectable.on.props.down = (t, val) => t.set({ touchstart: val })
+  injectable.on.props.up = (t, val) => t.set({ touchend: val })
+} else {
+  injectable.on.props.move = (t, val) => t.set({ mousemove: val })
+  // injectable.on.props.down = (t, val) => t.set({ mousedown: val })
+  injectable.on.props.up = (t, val) => t.set({ mouseup: val })
+}
 
 injectable.props = {
   hasEvents: {
